f3987aeacd7290eaa9e452530b394a9aa7a56e54
linszay <5556@holbertonstudents.com>
2022-09-15 15:47:24 -0700

    removed 2 from %d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 
 /**
  * main - printf
- * return: always 0
+ * Return: always 0
 */
 
 int main(void)
 {
-	printf("char is %2d bytes \n", sizeof(char));
-	printf("int is %2d bytes \n", sizeof(short int));
-	printf("long int is %2d bytes \n", sizeof(long int));
-	printf("long long int is %2d bytes \n", sizeof(long long int));
-	printf("float is %2d bytes \n", sizeof(float));
+	printf("char is %d bytes \n", sizeof(char));
+	printf("int is %d bytes \n", sizeof(short int));
+	printf("long int is %d bytes \n", sizeof(long int));
+	printf("long long int is %d bytes \n", sizeof(long long int));
+	printf("float is %d bytes \n", sizeof(float));
 	return (0);
 }
