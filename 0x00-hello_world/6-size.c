85c1371610b2ffa2c9856bd70f4a3fa8879476bf
linszay <5556@holbertonstudents.com>
2022-09-15 15:51:56 -0700

    added size of a to each printf line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("char is %d bytes \n", sizeof(char));
-	printf("int is %d bytes \n", sizeof(short int));
-	printf("long int is %d bytes \n", sizeof(long int));
-	printf("long long int is %d bytes \n", sizeof(long long int));
-	printf("float is %d bytes \n", sizeof(float));
+	printf("Size of a char is %d bytes \n", sizeof(char));
+	printf("Size of an int is %hd bytes \n", sizeof(short int));
+	printf("Size of a long int is %d bytes \n", sizeof(long int));
+	printf("Size of a long long int is %d bytes \n", sizeof(long long int));
+	printf("Size of a float is %d bytes \n", sizeof(float));
 	return (0);
 }
