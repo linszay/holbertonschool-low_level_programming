d0e6e228fc3f2735ed78d6e39451e9c9bad7d019
linszay <5556@holbertonstudents.com>
2022-09-15 12:40:10 -0700

    modified script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ *  main - puts
+ *
+ *  Return: always 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
