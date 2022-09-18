36e4ad3a55a30d9b14aa6e8ff6e582d9cf926262
linszay <lindseyjlancaster@gmail.com>
2022-09-18 16:56:35 -0500

    removed extra strings

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** more headers goes there */
 /**
- * main: void
+ * main - void
+ *
  * Description: if loop
  * Return: always 0
 */
-/** betty style doc for function main goes there */
 int main(void)
 {
-	char str[] = "Last digit of";
-	char is[] = "is";
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
 
 	/* your code goes there */
 	if (n > 5)
-		printf("%s %d %s and is greater than 5\n", str, n, is);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else if (n == 0)
-		printf("%s %d %s and is 0\n", str, n, is);
-	else if (n < 6 && n != 0)
-		printf("%s %d %s and is less than 6 and not 0\n", str, n, is);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	return (0);
 }
