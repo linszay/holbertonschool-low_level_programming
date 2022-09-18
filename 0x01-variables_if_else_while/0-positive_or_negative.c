40aa9033c11c0eb4021233309466ef61bcb9de9e
linszay <5556@holbertonstudents.com>
2022-09-17 18:36:41 -0700

    fixed trailing statements & compiled file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if ($n < 0)
-	{
+	if (n < 0)
+		; {
 		printf("$n is positive\n")
 	}
-	else if ($n == 0)
-	{
+	else if (n == 0)
+		; {
 		printf("$n is zero\n")
 	}
-	else if ($n > 0)
+	else if (n > 0)
+		; {
 		printf("$n is negative\n")
+	}
 	return (0);
 }
