1f5aa6e2423752e725874c2515d21187a0968424
linszay <5556@holbertonstudents.com>
2022-09-18 09:25:53 -0700

    changed return funtions back to printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,12 +20,14 @@ int main(void)
 
 	if (n < 0)
 	{
-		return ("%d is positive\n, n");
+		printf("%d is positive\n, n");
 	} else if (n == 0)
 	{
-		return ("&d is zero\n, n");
+		printf("&d is zero\n, n");
 	}
 	else if (n > 0)
 	{
-		return ("%d is negative\n, n");
+		printf("%d is negative\n, n");
 	}
+	return (0);
+}
