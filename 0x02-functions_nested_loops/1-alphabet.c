6eb14f47d9f8ac19c80473239d511c115a832611
linszay <lindseyjlancaster@gmail.com>
2022-09-19 16:22:02 -0500

    print alphabet with new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - void
+ * _putchar to print function
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
