104e89fe20c0bf83a42930547bb2956bd178e683
linszay <lindseyjlancaster@gmail.com>
2022-09-18 20:24:59 -0500

    for loop printing single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - void
+ * printf - looping to print 1-10
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 9; d++)
+	{
+		printf("%d", d);
+	}
+	printf("\n");
+
+	return (0);
+}
