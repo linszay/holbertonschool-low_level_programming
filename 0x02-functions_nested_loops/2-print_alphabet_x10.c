fe8f2d1fc084eee109e79593f1bd96713c71604f
linszay <lindseyjlancaster@gmail.com>
2022-09-19 17:47:43 -0500

    recompiled file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,9 @@ void print_alphabet_x10(void)
 	int ch;
 
 	for (n = 0; n < 10; n++)
-
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-	_putchar('\n');
+		_putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
 }
