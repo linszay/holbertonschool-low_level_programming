77b549d491b6a71e92839cd048151c4922df6727
linszay <lindseyjlancaster@gmail.com>
2022-09-20 13:38:03 -0500

    print last digit of random number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: test integer
+ * Return: last digit
+*/
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = (n % 10);
+	}
+	_putchar(n + '0');
+	return (n);
+}
