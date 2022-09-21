a7c57ee22e5a5448ce9e2e39e8cbb64d7021ec4c
linszay <lindseyjlancaster@gmail.com>
2022-09-20 20:08:35 -0500

    refactored 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,11 @@ int main(void)
 	for (d = 0; d < 10; d++)
 	{
 		putchar((d % 10) + '0');
-		if (d == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
