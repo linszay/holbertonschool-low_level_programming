e3008d2457f047d56c86b55d95429c7eaf9ddf74
linszay <lindseyjlancaster@gmail.com>
2022-09-20 22:34:43 -0500

    print numbers to 98 and back

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints to 98
+ * @n: test integer
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = 0; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		printf("%d, ", n);
+	}
+}
