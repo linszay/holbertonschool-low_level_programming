664b31b99cb912ffb6d34d0381a87068bff989a3
linszay <lindseyjlancaster@gmail.com>
2022-09-22 11:26:39 -0500

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints out numbers 0-9
+ * @i: integer for loop
+ * _putchar - prints character 
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = i; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar("\n");
+}
