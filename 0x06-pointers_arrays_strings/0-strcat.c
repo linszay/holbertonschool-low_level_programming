670758d3a6d9e0baf39403147c63b22d58f76507
linszay <lindseyjlancaster@gmail.com>
2022-09-30 11:19:09 -0500

    added betty formatting

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strcat - appends src to dest string 
+ * _strcat - appends src to dest string
  * and prints to null character
  * @dest: first string
  * @src: string to be appended
