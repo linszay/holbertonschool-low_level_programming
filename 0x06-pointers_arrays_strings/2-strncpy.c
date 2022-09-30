3e4eb3e4f31169054de7780a6958ed2617941712
linszay <lindseyjlancaster@gmail.com>
2022-09-30 15:38:17 -0500

    added to comment

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- * *_strncpy - copies a string
+ * *_strncpy - copies a string according to number of bytes
  * @dest: copy string here
  * @src: string to be copied
+ * @n: number of bytes
  * Return: 0
 */
 
