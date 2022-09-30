cde49cd57a4287ea33619263c106087c6558e8da
linszay <lindseyjlancaster@gmail.com>
2022-09-30 15:37:21 -0500

    modified where incrementing was at

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	while (src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
+		dest[i] = src[i];
 		i++;
 	}
 	while (i < n)
 	{
-		dest[i] = src[i];
-		dest[i++] = '\0';
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
