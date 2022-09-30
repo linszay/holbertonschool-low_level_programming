bf6b2622b34e942846c624966e13cf8267b46071
linszay <lindseyjlancaster@gmail.com>
2022-09-30 14:25:41 -0500

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	if (i < n)
-	{
-		dest[i] = src[i];
-	}
 	while (i < n)
 	{
+		dest[i] = src[i];
 		dest[i++] = '\0';
 	}
 	return (dest);
