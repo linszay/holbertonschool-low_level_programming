c71171feca5d739b9cfd710e83b75eaf09fd9854
linszay <lindseyjlancaster@gmail.com>
2022-10-03 14:27:28 -0500

    capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * *cap_string - capitalizes all words of a sing
+ * @s: pointer to sing
+ * Return: sing
+*/
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+		if (i == 0)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+			{
+				s[i] -= 32;
+			}
+		}
+		if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+		    s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+		    s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+		    s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+		    s[i - 1] == '}' || i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] -= 32;
+			}
+		}
+	}
+	return (s);
+}
