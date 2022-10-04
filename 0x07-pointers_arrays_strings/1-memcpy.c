cdd49a9dd8f30f641f820d6ccf22fc285e1a1934
linszay <lindseyjlancaster@gmail.com>
2022-10-04 00:16:43 -0500

    copies bytes from one source to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - copies bytes from memory src
+ * to memory dest
+ * @dest: byte destination
+ * @src: input to copy
+ * @n: unsigned int
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
