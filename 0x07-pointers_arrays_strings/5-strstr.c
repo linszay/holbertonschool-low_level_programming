898040762211c74daad9f988ec1ed5ef101e5364
linszay <lindseyjlancaster@gmail.com>
2022-10-04 23:15:28 -0500

    needle in haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - finds first occurrence of needle in
+ * string haystack
+ * @haystack: input
+ * @needle: input
+ * Return: pointer or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *hay = haystack;
+		char *needle = needle;
+
+		while (*hay == *needle && *needle != '\0')
+		{
+			hay++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+	}
+
+	return ('\0');
+}
