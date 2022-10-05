b5a1992807d659948814be35c44be9bf2e38a414
linszay <lindseyjlancaster@gmail.com>
2022-10-04 23:34:37 -0500

    added betty doc styling

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,8 @@ char *_strstr(char *haystack, char *needle)
 		temp = haystack;
 		while (*needle == *haystack)
 		{
-			haystack++, needle++;
+			haystack++;
+			needle++;
 		}
 		if (*needle == '\0')
 			return (temp);
