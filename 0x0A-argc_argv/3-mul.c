59aff0da2cd11fa36a9945ec6acb74efca005520
linszay <lindseyjlancaster@gmail.com>
2022-10-11 21:18:05 -0500

    multiplies two numbers or returns 1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers or returns 1
+ * @argc: argument counter
+ * @agrv: argument vector
+ * Return: result or 1
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		multiplication = multiplication * atoi(argv[i]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
