7ca623a6abd406fd2233e17fd1326f8b80eb274b
linszay <lindseyjlancaster@gmail.com>
2022-10-11 22:20:50 -0500

    modified 0 to NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	s = (char *)malloc(size * sizeof(char));
-	if (s == '\0')
+	if (s == NULL)
 		return ('\0');
 	for (a = 0; a < size; a++)
 	{
