f417838aa0d7507b9165ca2b3eef726eb2367b47
linszay <lindseyjlancaster@gmail.com>
2022-10-11 23:10:13 -0500

    returns pointer to 2 dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,15 +3,14 @@
 #include "main.h"
 
 /**
- * **alloc_grid - concatenates two strings and
- * returns pointer to newly allocated space in
- * memory with contents of s1 and s2 then null terminated
+ * **alloc_grid - returns pointer to two-dimensional
+ * array of integers
  * @width: columns of array
  * @height: rows of array
  * Return: pointer or null
  */
 
-int **alloc_grid(int width, int height) /* 6 columns and 4 rows*/
+int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **arr;
