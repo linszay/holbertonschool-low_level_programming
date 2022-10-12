89eb40d8dd2f0660e780a4d5dd15910e92c18eca
linszay <lindseyjlancaster@gmail.com>
2022-10-11 19:27:50 -0500

    program prints name and newline

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints file name and newline
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: void
+*/
+
+void main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
+}
