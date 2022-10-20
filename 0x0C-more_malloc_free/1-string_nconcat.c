52fc50343bfe65e95a0d85bd7e66e4a678cc69f8
linszay <lindseyjlancaster@gmail.com>
2022-10-19 20:41:14 -0500

    swapped placement of line 37 and 38

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,8 +34,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > j)
 	{
 		n = j;
-		p = malloc(((i + n) + 1));
 	}
+	p = malloc(((i + n) + 1));
 	if (p == NULL)
 	{
 		return (NULL);
