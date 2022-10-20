607934a3d4f9d7ff84c22218d8cfd96cfa757288
linszay <lindseyjlancaster@gmail.com>
2022-10-20 13:07:26 -0500

    added betty doc styling to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,20 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - contains dog related data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+*/
+
 typedef struct dog
 {
     /* data */
-    char *name;
-    float age;
-    char *owner;
-}my_dog;
+char *name;
+float age;
+char *owner;
+} my_dog;
 
 int main(void);
 
