6980789942f9ebdcc7abce8640a38ece53c36a5b
linszay <lindseyjlancaster@gmail.com>
2022-10-20 13:04:10 -0500

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains programs that utilize typedef and structs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef dog_h
+#define dog_h
+
+typedef struct dog
+{
+    /* data */
+    char *name;
+    float age;
+    char *owner;
+}my_dog;
+
+int main(void);
+
+#endif
