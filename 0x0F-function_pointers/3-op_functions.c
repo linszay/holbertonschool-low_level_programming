c79639675ddaac4cdb93b2a118d948112d817a96
linszay <lindseyjlancaster@gmail.com>
2022-10-25 13:32:19 -0500

    created programs to perform simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result of the sum of two numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction of two numbers
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result of the subtraction of two numbers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of two numbers
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: multiplication of two numbers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two numbers
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: result of the division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulation of two numbers
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: modulation of two numbers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
