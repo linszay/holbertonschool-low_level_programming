b0f2b8d4ff3f694077ac46bab6966dc8b6fc291b
linszay <lindseyjlancaster@gmail.com>
2022-11-06 18:02:36 -0600

    adds a new node at the end of list_t

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,4 +8,33 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	int i, count = 0;
+	list_t *new;
+	list_t *temp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
