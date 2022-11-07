a91fff4980ad8b87dd0c20e47d8ab880779dd7d1
linszay <lindseyjlancaster@gmail.com>
2022-11-06 22:59:16 -0600

    frees listint_t and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees listint_t and sets head to
+ * NULL
+ * @head: pointer to list
+ */
+void free_listint2(listint_t **head)
+{
+	while (*head)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+
+	*head = NULL;
+}
