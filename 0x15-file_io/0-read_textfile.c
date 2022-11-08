ec4bf9424faf0a049f473cc63fbf63687f678799
linszay <lindseyjlancaster@gmail.com>
2022-11-08 16:21:17 -0600

    creates a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * read_textfile - Entry Point
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
  * @filename: file name
  * @letters: size
  * Return: 0
