e6b6b3fa9fdf941dd0c4ef43671979ad463c7062
linszay <lindseyjlancaster@gmail.com>
2022-11-08 13:48:50 -0600

    added betty doc styling and clear_bit file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * set_bit - sets the value of a bit to 1 at 
- * a given index
+ * clear_bit - sets the bit value to 0 at the index
  * @n: unsigned long int
  * @index: index
  * Return: 1 or -1
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bits;
 
@@ -15,7 +14,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	bits = 1 << index;
 
-	*n = *n | bits;
+	if ((bits | *n) == *n)
+		*n = *n ^ bits;
 
 	return (1);
 }
