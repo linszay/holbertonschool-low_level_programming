4a4ed23a6aacc3d558f55c36e5f23d987ad1eba1
linszay <lindseyjlancaster@gmail.com>
2022-11-07 20:20:19 -0600

    testing file again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - Entry Point
- * @head: head
- * @idx: index
+ * insert_nodeint_at_index - inserts a new node at a
+ * given position in the list
+ * @head: pointer to list
+ * @idx: position in the list
  * @n: data
  * Return: 0
  */
@@ -11,14 +12,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 
 	listint_t *new;
-	listint_t *temp;
+	listint_t *tmp;
 
 	if (*head == NULL)
 		return (NULL);
 
-	temp = *head;
+	tmp = *head;
 
-	while (temp != NULL)
+	while (tmp != NULL)
 	{
 		if (count == idx - 1)
 		{
@@ -30,13 +31,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			else
 			{
 				new->n = n;
-				new->next = temp->next;
-				temp->next = new;
+				new->next = tmp->next;
+				tmp->next = new;
 				return (new);
 			}
 		}
-		if (temp->next != NULL)
-			temp = temp->next;
+		if (tmp->next != NULL)
+			tmp = tmp->next;
 		count++;
 	}
 	return (NULL);
