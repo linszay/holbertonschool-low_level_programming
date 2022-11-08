3294b849c25d667912fd507fcd5adb2fd0314708
linszay <lindseyjlancaster@gmail.com>
2022-11-08 13:21:37 -0600

    added betty doc styling

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * binary_to_uint - converts a binary number to an
  * unsigned int
  * @b: const char
  * Return: converted number or 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui = 0;
