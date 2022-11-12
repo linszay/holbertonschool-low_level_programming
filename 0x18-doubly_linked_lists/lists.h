e7055efdf808594d1b4f0d8a449c12da42c067fd
linszay <lindseyjlancaster@gmail.com>
2022-11-12 17:24:43 -0600

    added stdlib.h to header file

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -13,9 +14,9 @@
  */
 typedef struct dlistint_s
 {
-int n;
-struct dlistint_s *prev;
-struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
