6c4acabe0fabf9106728feccd12a31889c284387
linszay <lindseyjlancaster@gmail.com>
2022-11-12 17:08:19 -0600

    returns number of elements of list

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistinst_len - returns the number of elements
+ * in a linked list dlistint_t
+ * @h: const pointer to list
+ * Return: number of elements in the list
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
