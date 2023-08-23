248a6b82f8e64c9208607569d469590c33a00bd4
linszay <lindseyjlancaster@gmail.com>
2023-08-23 03:36:03 -0500

    repushing with updates to code

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,13 +5,16 @@ int linear_search(int *array, size_t size, int value) {
         return -1; 
     }
 
-    for (size_t i = 0; i < size; i++) {
+    {
+        
+        size_t i;
+    for (i = 0; i < size; i++) {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         
         if (array[i] == value) {
             return (int)i;
         }
     }
-
+    }
     return -1;
 }
